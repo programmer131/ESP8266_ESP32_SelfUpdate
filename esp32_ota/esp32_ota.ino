880d4430fcde6cb92c6b697f082e6b412fa98b5f
Farrukh <farrukh.h@dplit.com>
2020-11-21 18:25:29 +0500

    updated url and ota procedure

--- esp32_ota/esp32_ota.ino
+++ esp32_ota/esp32_ota.ino
@@ -8,8 +8,8 @@
 
 #define AIO_SERVER      "io.adafruit.com"
 #define AIO_SERVERPORT  1883                   // use 8883 for SSL
-#define AIO_USERNAME    "programmer5"
-#define AIO_KEY         ""
+#define AIO_USERNAME    "programmer5" //enter your own adafruit io user name
+#define AIO_KEY         ""//enter your own adafruit io key
 
 WiFiClient client_mqtt;
 Adafruit_MQTT_Client mqtt(&client_mqtt, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO_KEY);
@@ -18,8 +18,8 @@ Adafruit_MQTT_Publish device_log = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/f
 void MQTT_connect();
  
 const String FirmwareVer={"0.2"}; 
-#define URL_fw_Version "https://raw.githubusercontent.com/programmer131/ESP8266_ESP32_SelfUpdate/esp32_bin/master/bin_version.txt"
-#define URL_fw_Bin "https://raw.githubusercontent.com/programmer131/ESP8266_ESP32_SelfUpdate/esp32_bin/master/esp32.bin"
+#define URL_fw_Version "https://raw.githubusercontent.com/programmer131/ESP8266_ESP32_SelfUpdate/master/esp32_bin/bin_version.txt"
+#define URL_fw_Bin "https://raw.githubusercontent.com/programmer131/ESP8266_ESP32_SelfUpdate/master/esp32_bin/fw.bin"
 
 //#define URL_fw_Version "http://cade-make.000webhostapp.com/version.txt"
 //#define URL_fw_Bin "http://cade-make.000webhostapp.com/firmware.bin"
@@ -49,8 +49,29 @@ const char* rootCACertificate = \
 "vEsXCS+0yx5DaMkHJ8HSXPfqIbloEpw8nL+e/IBcm2PN7EeqJSdnoDfzAIJ9VNep\n"
 "+OkuE6N36B9K\n"
 "-----END CERTIFICATE-----\n";
- 
-void FirmwareUpdate()
+
+void firmwareUpdate(void)
+{
+    WiFiClientSecure client;
+    client.setCACert(rootCACertificate);
+    httpUpdate.setLedPin(LED_BUILTIN, LOW);   
+    t_httpUpdate_return ret = httpUpdate.update(client,URL_fw_Bin);
+    
+    switch (ret) {
+      case HTTP_UPDATE_FAILED:
+        Serial.printf("HTTP_UPDATE_FAILD Error (%d): %s\n", httpUpdate.getLastError(), httpUpdate.getLastErrorString().c_str());
+        break;
+
+      case HTTP_UPDATE_NO_UPDATES:
+        Serial.println("HTTP_UPDATE_NO_UPDATES");
+        break;
+
+      case HTTP_UPDATE_OK:
+        Serial.println("HTTP_UPDATE_OK");
+        break;
+    }   
+}
+int FirmwareVersionCheck(void)
 {
   http.begin(URL_fw_Version,"CC AA 48 48 66 46 0E 91 53 2C 9C 7C 23 2A B1 74 4D 29 9D 33");     // check version URL
   delay(100);
@@ -60,8 +81,6 @@ void FirmwareUpdate()
   if (httpCode == HTTP_CODE_OK)         // if version received
   {
     payload = http.getString();  // save received version
-    Serial.print("payload:");
-    Serial.println(payload );
   }
   else
   {
@@ -75,54 +94,45 @@ void FirmwareUpdate()
     if(payload.equals(FirmwareVer) )
     {   
        Serial.println("Device already on latest firmware version"); 
+       return 0;
     }
     else
     {
-      Serial.print("payload:");
+      Serial.print("version file payload:");
       Serial.println(payload );
-      Serial.print("FirmwareVer:");
-      Serial.println(FirmwareVer );
       Serial.println("New firmware detected");
-      WiFiClientSecure client;
-      client.setCACert(rootCACertificate);
-      // The line below is optional. It can be used to blink the LED on the board during flashing
-      // The LED will be on during download of one buffer of data from the network. The LED will
-      // be off during writing that buffer to flash
-      // On a good connection the LED should flash regularly. On a bad connection the LED will be
-      // on much longer than it will be off. Other pins than LED_BUILTIN may be used. The second
-      // value is used to put the LED on. If the LED is on with HIGH, that value should be passed
-      httpUpdate.setLedPin(LED_BUILTIN, LOW);
-  
-  
-      t_httpUpdate_return ret = httpUpdate.update(client,URL_fw_Bin);
-      
-      switch (ret) {
-        case HTTP_UPDATE_FAILED:
-          Serial.printf("HTTP_UPDATE_FAILD Error (%d): %s\n", httpUpdate.getLastError(), httpUpdate.getLastErrorString().c_str());
-          break;
-  
-        case HTTP_UPDATE_NO_UPDATES:
-          Serial.println("HTTP_UPDATE_NO_UPDATES");
-          break;
-  
-        case HTTP_UPDATE_OK:
-          Serial.println("HTTP_UPDATE_OK");
-          break;
-      } 
+      return 1;
     }
- }  
+  }
 }
+      
+    
 
 unsigned long previousMillis = 0;        // will store last time LED was updated
+unsigned long previousMillis_2 = 0; 
 const long interval = 40000;
-
+const long one_sec_interval=1000;
  void repeatedCall(){
     unsigned long currentMillis = millis();
     if ((currentMillis - previousMillis) >= interval) 
     {
       // save the last time you blinked the LED
       previousMillis = currentMillis;
-      FirmwareUpdate();
+      if(FirmwareVersionCheck())
+      {
+        firmwareUpdate();
+      }
+    }
+    if ((currentMillis - previousMillis_2) >= one_sec_interval) 
+    {
+      previousMillis_2=currentMillis;
+      Serial.printf(".%d",millis()/1000);
+      if((millis()/1000)%10==0)
+      {
+        Serial.print("FirmwareVer:");
+        Serial.println(FirmwareVer );
+        Serial.println();
+      }
     }
  }
 
@@ -133,11 +143,11 @@ struct Button {
 };
 
 Button button_boot = {0, 0, false};
-void IRAM_ATTR isr(void* arg) {
+/*void IRAM_ATTR isr(void* arg) {
     Button* s = static_cast<Button*>(arg);
     s->numberKeyPresses += 1;
     s->pressed = true;
-}
+}*/
 
 void IRAM_ATTR isr() {
     button_boot.numberKeyPresses += 1;
@@ -147,14 +157,12 @@ void connect_wifi()
 {
   WiFi.mode(WIFI_AP_STA);
   WiFi.beginSmartConfig();
-
   //Wait for SmartConfig packet from mobile
   Serial.println("Waiting for SmartConfig.");
   while (!WiFi.smartConfigDone()) {
     delay(500);
     Serial.print(".");
   }
-
   Serial.println("");
   Serial.println("SmartConfig received.");
 
@@ -170,16 +178,21 @@ void connect_wifi()
   Serial.print("IP Address: ");
   Serial.println(WiFi.localIP());
 }
+int do_ota=0;
+void ota_button_cb(char *data, uint16_t len) {
+  do_ota=1;
+}
 void setup()
 {
   pinMode(button_boot.PIN, INPUT);
-  attachInterruptArg(button_boot.PIN, isr, &button_boot, RISING);
+  attachInterrupt(button_boot.PIN, isr, RISING);
   Serial.begin(115200);
   WiFi.begin();
   delay(1000);
   Serial.print("Active firmware version:");
   Serial.println(FirmwareVer);
   pinMode(LED_BUILTIN, OUTPUT);
+  ota_button.setCallback(ota_button_cb);
   mqtt.subscribe(&ota_button);
 }
 void loop()
@@ -189,11 +202,12 @@ void loop()
         button_boot.pressed = false;
   }
   MQTT_connect();
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);                       // wait for a second
-  repeatedCall();    
+  repeatedCall(); 
+  if(do_ota)
+  {
+    do_ota=0;
+    firmwareUpdate();
+  }
 }
 
 
@@ -214,4 +228,5 @@ void MQTT_connect() {
        return;
   }
   Serial.println("MQTT Connected!");
+ 
 }
