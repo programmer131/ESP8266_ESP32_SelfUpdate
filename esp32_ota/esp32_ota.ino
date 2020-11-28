78c186fb25d3f031fb4a571c76a4ad1d001003b9
farrukh <farrukh.h@dplit.com>
2020-11-28 15:58:00 +0500

    v2.2

--- esp32_ota/esp32_ota.ino
+++ esp32_ota/esp32_ota.ino
@@ -9,7 +9,7 @@ const char * password = "helloworld";
 
 
 String FirmwareVer = {
-  "2.1"
+  "2.2"
 };
 #define URL_fw_Version "https://raw.githubusercontent.com/programmer131/ESP8266_ESP32_SelfUpdate/master/esp32_ota/bin_version.txt"
 #define URL_fw_Bin "https://raw.githubusercontent.com/programmer131/ESP8266_ESP32_SelfUpdate/master/esp32_ota/fw.bin"
