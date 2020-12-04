d435caf277986f16cae7d193b7645dace48f19fb
farrukh <farrukh.h@dplit.com>
2020-12-04 22:38:04 +0500

    removed 2nd esp8266 ota example, added certificate instead of fingerprint for esp8266 ota

---
+++ esp8266_ota/esp8266_ota.ino
@@ -0,0 +1,173 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <ESP8266httpUpdate.h>
+#include <WiFiClientSecure.h>
+#include <CertStoreBearSSL.h>
+BearSSL::CertStore certStore;
+#include <time.h>
+ 
+const String FirmwareVer={"1.8"}; 
+#define URL_fw_Version "/programmer131/otaFiles/master/version.txt"
+#define URL_fw_Bin "https://raw.githubusercontent.com/programmer131/otaFiles/master/firmware.bin"
+const char* host = "raw.githubusercontent.com";
+const int httpsPort = 443;
+
+// DigiCert High Assurance EV Root CA
+const char trustRoot[] PROGMEM = R"EOF(
+-----BEGIN CERTIFICATE-----
+MIIDxTCCAq2gAwIBAgIQAqxcJmoLQJuPC3nyrkYldzANBgkqhkiG9w0BAQUFADBs
+MQswCQYDVQQGEwJVUzEVMBMGA1UEChMMRGlnaUNlcnQgSW5jMRkwFwYDVQQLExB3
+d3cuZGlnaWNlcnQuY29tMSswKQYDVQQDEyJEaWdpQ2VydCBIaWdoIEFzc3VyYW5j
+ZSBFViBSb290IENBMB4XDTA2MTExMDAwMDAwMFoXDTMxMTExMDAwMDAwMFowbDEL
+MAkGA1UEBhMCVVMxFTATBgNVBAoTDERpZ2lDZXJ0IEluYzEZMBcGA1UECxMQd3d3
+LmRpZ2ljZXJ0LmNvbTErMCkGA1UEAxMiRGlnaUNlcnQgSGlnaCBBc3N1cmFuY2Ug
+RVYgUm9vdCBDQTCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBAMbM5XPm
++9S75S0tMqbf5YE/yc0lSbZxKsPVlDRnogocsF9ppkCxxLeyj9CYpKlBWTrT3JTW
+PNt0OKRKzE0lgvdKpVMSOO7zSW1xkX5jtqumX8OkhPhPYlG++MXs2ziS4wblCJEM
+xChBVfvLWokVfnHoNb9Ncgk9vjo4UFt3MRuNs8ckRZqnrG0AFFoEt7oT61EKmEFB
+Ik5lYYeBQVCmeVyJ3hlKV9Uu5l0cUyx+mM0aBhakaHPQNAQTXKFx01p8VdteZOE3
+hzBWBOURtCmAEvF5OYiiAhF8J2a3iLd48soKqDirCmTCv2ZdlYTBoSUeh10aUAsg
+EsxBu24LUTi4S8sCAwEAAaNjMGEwDgYDVR0PAQH/BAQDAgGGMA8GA1UdEwEB/wQF
+MAMBAf8wHQYDVR0OBBYEFLE+w2kD+L9HAdSYJhoIAu9jZCvDMB8GA1UdIwQYMBaA
+FLE+w2kD+L9HAdSYJhoIAu9jZCvDMA0GCSqGSIb3DQEBBQUAA4IBAQAcGgaX3Nec
+nzyIZgYIVyHbIUf4KmeqvxgydkAQV8GK83rZEWWONfqe/EW1ntlMMUu4kehDLI6z
+eM7b41N5cdblIZQB2lWHmiRk9opmzN6cN82oNLFpmyPInngiK3BD41VHMWEZ71jF
+hS9OMPagMRYjyOfiZRYzy78aG6A9+MpeizGLYAiJLQwGXFK3xPkKmNEVX58Svnw2
+Yzi9RKR/5CYrCsSXaQ3pjOLAEFe4yHYSkVXySGnYvCoCWw9E1CAx2/S6cCZdkGCe
+vEsXCS+0yx5DaMkHJ8HSXPfqIbloEpw8nL+e/IBcm2PN7EeqJSdnoDfzAIJ9VNep
++OkuE6N36B9K
+-----END CERTIFICATE-----
+)EOF";
+X509List cert(trustRoot);
+
+
+extern const unsigned char caCert[] PROGMEM;
+extern const unsigned int caCertLen;
+
+const char* ssid = "home_wifi";
+const char* password = "helloworld";
+
+void setClock() {
+   // Set time via NTP, as required for x.509 validation
+  configTime(3 * 3600, 0, "pool.ntp.org", "time.nist.gov");
+  Serial.print("Waiting for NTP time sync: ");
+  time_t now = time(nullptr);
+  while (now < 8 * 3600 * 2) {
+    delay(500);
+    Serial.print(".");
+    now = time(nullptr);
+  }
+  /*
+  Serial.println("");
+  struct tm timeinfo;
+  gmtime_r(&now, &timeinfo);
+  Serial.print("Current time: ");
+  Serial.print(asctime(&timeinfo));
+  */
+}
+  
+void FirmwareUpdate()
+{  
+  WiFiClientSecure client;
+  client.setTrustAnchors(&cert);
+  if (!client.connect(host, httpsPort)) {
+    Serial.println("Connection failed");
+    return;
+  }
+  client.print(String("GET ") + URL_fw_Version + " HTTP/1.1\r\n" +
+               "Host: " + host + "\r\n" +
+               "User-Agent: BuildFailureDetectorESP8266\r\n" +
+               "Connection: close\r\n\r\n");
+  while (client.connected()) {
+    String line = client.readStringUntil('\n');
+    if (line == "\r") {
+      //Serial.println("Headers received");
+      break;
+    }
+  }
+  String payload = client.readStringUntil('\n');
+
+  payload.trim();
+  if(payload.equals(FirmwareVer) )
+  {   
+     Serial.println("Device already on latest firmware version"); 
+  }
+  else
+  {
+    Serial.println("New firmware detected");
+    ESPhttpUpdate.setLedPin(LED_BUILTIN, LOW); 
+    t_httpUpdate_return ret = ESPhttpUpdate.update(client, URL_fw_Bin);
+        
+    switch (ret) {
+      case HTTP_UPDATE_FAILED:
+        Serial.printf("HTTP_UPDATE_FAILD Error (%d): %s\n", ESPhttpUpdate.getLastError(), ESPhttpUpdate.getLastErrorString().c_str());
+        break;
+
+      case HTTP_UPDATE_NO_UPDATES:
+        Serial.println("HTTP_UPDATE_NO_UPDATES");
+        break;
+
+      case HTTP_UPDATE_OK:
+        Serial.println("HTTP_UPDATE_OK");
+        break;
+    } 
+  }
+ }  
+void connect_wifi();
+unsigned long previousMillis_2 = 0;
+unsigned long previousMillis = 0;        // will store last time LED was updated
+const long interval = 60000;
+const long mini_interval = 1000;
+ void repeatedCall(){
+    unsigned long currentMillis = millis();
+    if ((currentMillis - previousMillis) >= interval) 
+    {
+      // save the last time you blinked the LED
+      previousMillis = currentMillis;
+      setClock();
+      FirmwareUpdate();
+    }
+
+    if ((currentMillis - previousMillis_2) >= mini_interval) {
+      static int idle_counter=0;
+      previousMillis_2 = currentMillis;    
+      Serial.print(" Active fw version:");
+      Serial.println(FirmwareVer);
+      Serial.print("Idle Loop....");
+      Serial.println(idle_counter++);
+     if(idle_counter%2==0)
+      digitalWrite(LED_BUILTIN, HIGH);
+     else 
+      digitalWrite(LED_BUILTIN, LOW);
+     if(WiFi.status() == !WL_CONNECTED) 
+          connect_wifi();
+   }
+ }
+
+  
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println("");
+  Serial.println("Start");
+  WiFi.mode(WIFI_STA);
+  void connect_wifi();  
+  setClock();
+  pinMode(LED_BUILTIN, OUTPUT);
+  
+}
+void loop()
+{
+  repeatedCall();    
+}
+
+void connect_wifi()
+{
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print("O");
+  }                                   
+  Serial.println("Connected to WiFi");
+}
